4756a38e5c08df726698982c304b8d8af34690c3
rafaelbelda <rafaelgbelda@gmail.com>
2026-02-06 18:48:48 -0300

    led fix

--- src/hardware/led_recording.py
+++ src/hardware/led_recording.py
@@ -3,7 +3,7 @@ import time
 import threading
 
 LED_PIN = 7
-INTERVAL_SECONDS_SHORT = 0.15
+INTERVAL_SECONDS_SHORT = 0.25
 INTERVAL_SECONDS_LONG = 0.5
 
 GPIO.setmode(GPIO.BCM)
@@ -14,12 +14,12 @@ _blink_thread = None
 
 def _blink_loop():
     while not _stop_event.is_set():
-         GPIO.output(LED_PIN, GPIO.HIGH) # ON
-         time.sleep(INTERVAL_SECONDS_LONG)
-         GPIO.output(LED_PIN, GPIO.LOW) # OFF
+        GPIO.output(LED_PIN, GPIO.HIGH) # ON
+        time.sleep(INTERVAL_SECONDS_SHORT)
+        GPIO.output(LED_PIN, GPIO.LOW) # OFF
  
-         #time.sleep(INTERVAL_SECONDS)
-         #time.sleep(INTERVAL_SECONDS_LONG)
+        #time.sleep(INTERVAL_SECONDS)
+        #time.sleep(INTERVAL_SECONDS_LONG)
 
 
 def start_blinking():
